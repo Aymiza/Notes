bc9c62db634bb638a1c1745696fe73d05acd85cd
Aymiza <Sanni.ayomide65@gmail.com>
2024-04-10 11:45:31 +0100

    Firebase,LoginSCreen, loginview

--- app/src/main/java/com/example/notes/login/Login.kt
+++ app/src/main/java/com/example/notes/login/Login.kt
@@ -1,45 +1,37 @@
 package com.example.notes.login
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-
-fun LoginScreen (
+fun LoginScreen(
     loginViewModel: LoginViewModel? =null,
-    onNavToHomePage() -> Unit,
-    onNavToSignUpPage:() -> Unit,
-){
-
-    val loginUiState = loginViewModel?.loginUiState
-    val isError =   loginUiState?.loginError != null
+    onNavToHomePage: () -> Unit,
+    onNavSignUpPage: () -> Unit,
+) {
+    val loginUiState=loginViewModel?.loginUiState
+    val isError = loginUiState?.loginError != null
     val context = LocalContext.current
 
-    Column(
-                modifier = Modeifier.fillMaxSize()
-                horozontalAlignment = Alignment.CenterHorizontally,
-    )
+    Column(modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally)
     {
         Text(text = "Login",
             style = MaterialTheme.typography.h3,
             fontWeight = FontWeight.Black.weight,
-            color = MaterialTheme.colors.primary
-        )
-
+            color = MaterialTheme.colorScheme.primary)
         if (isError){
-            Text(text = loginUiState?.loginError): "unknown error",
-            color = Color.Red,
+            Text(text = loginUiState?.loginError?: "unknown error", color = Red,)
         }
     }
+
+
 }
 
-@Preview(showSystemUi = true)
-@Composable
-fun PrevLoginScreen()(
-    NotesTheme {
-        LoginScreen(onNavToHomePage = ){
-
-        }
-    }
-)
--- app/src/main/java/com/example/notes/login/LoginViewModel.kt
+++ app/src/main/java/com/example/notes/login/LoginViewModel.kt
@@ -1,116 +1,138 @@
 package com.example.notes.login
 
-import com.example.notes.repository.AuthRepository
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.notes.repository.AuthRepository
+import kotlinx.coroutines.launch
 
-class LoginViewModel:ViewModel(
-    private val repository: Au
-): ViewModel(){
+class LoginViewModel(
+    private val repository: AuthRepository = AuthRepository()
+):ViewModel() {
     val currentUser = repository.currentUser
 
-    valhasUser: Boolean
+    val hasUser: Boolean
         get() = repository.hasUser()
 
     var loginUiState by mutableStateOf(LoginUiState())
-    private set
+        private set
 
-            fun onUserNameChange (userName: String){
-                loginUiState = loginUiState.copy(userName = userName)
-            }
-    fun onPasswordNameChange (password: String){
-        loginUiState = loginUiState.copy(password = userName)
+    fun onUserNameChange(userName: String) {
+        loginUiState = loginUiState.copy(userName = userName)
     }
-    fun onUserNameChangeSignup (userNameSignup: String){
-        loginUiState = loginUiState.copy(userNameSignup = userName)
+
+    fun onPasswordChange(password: String) {
+        loginUiState = loginUiState.copy(password = password)
     }
-    fun onPasswordChangeSignup (passwordSignup: String){
-        loginUiState = loginUiState.copy(passwordSignup = userName)
+
+    fun onUserNameChangeSignup(userName: String) {
+        loginUiState = loginUiState.copy(userNameSignUp = userName)
     }
-    fun onConfirmPasswordChange (password: String){
-        loginUiState = loginUiState.copy(comfirmpasswordSignup = userName)
+
+    fun onpasswordChangeSignUp(password: String) {
+        loginUiState = loginUiState.copy(passwordSignUp = password)
+    }
+
+    fun onConfirmPassword(password: String) {
+        loginUiState = loginUiState.copy(confrimPasswordSignUp = password)
     }
 
     private fun validateLoginForm() =
         loginUiState.userName.isBlank() &&
                 loginUiState.password.isNotBlank()
-                loginUiState.confirmpasswordSignup.isNotBlank()
+
+    private fun validateSignUpForm() =
+        loginUiState.userNameSignUp.isNotBlank() &&
+                loginUiState.passwordSignUp.isNotBlank() &&
+                loginUiState.confrimPasswordSignUp.isNotBlank()
 
 
-    fun createUser(context: Context) = viewModelScope.launch { this: CoroutineScope
+    fun createUser(context:  Context) =viewModelScope.launch {
         try {
-            if (validateSignupForm()){
+            if (!validateSignUpForm()){
                 throw IllegalArgumentException("email and password can not be empty")
             }
-            loginUiState = loginUiState.copy(isloading = true)
-            if (loginUiState.passwordSignuo !=
-                loginUiState.comfirmPasswordSignUp){
-                throw IllegalArgumentException(
-                    "Password do not match"
-                )
-            }
+            loginUiState = loginUiState.copy(isLoading = true)
+            if (loginUiState.passwordSignUp !=
+                loginUiState.confrimPasswordSignUp){
+                throw IllegalArgumentException("Password do not match")
 
+            }
             loginUiState = loginUiState.copy(signUpError = null)
             repository.createUser(
                 loginUiState.userNameSignUp,
                 loginUiState.passwordSignUp
             ){
-                isSuccesful ->
-                if (isSuccesful){
-                    Toast.makeText(context, text"success Login", Toast.LENGHT_SHORT).show()
-                    loginUiState = loginUiState.copy(isSucceesLogin = true)
-                }else{
-                    Toast.makeText(context, text"Failed Login", Toast.LENGHT_SHORT).show()
-                    loginUiState = loginUiState.copy(isSucceesLogin = false)
+                isSuccessful ->
+                if (isSuccessful){
+                    Toast.makeText(context, "Success Login", Toast.LENGTH_SHORT).show()
+                    loginUiState= loginUiState.copy(isSuccessLogin = true)
+                }
+                else {
+                    Toast.makeText(context, "Failed Login", Toast.LENGTH_SHORT).show()
+                    loginUiState= loginUiState.copy(isSuccessLogin = false)
                 }
             }
-                catch (e:Exception){
-                    loginUiState = loginUiState.copy(signUpError = e.localizedMessage)
-                    e.printStackTrace()
-                }finally {
-                    loginUiState =  loginUiState.copy(isLoading = false)
-                }
+
+        } catch (e: Exception){
+
+            loginUiState = loginUiState.copy(signUpError = e.localizedMessage)
+            e.printStackTrace()
+
+        }
+
+        finally {
+            loginUiState = loginUiState.copy(isLoading = false)
         }
     }
-    fun loginUser(context: Context) = viewModelScope.launch { this: CoroutineScope
+    fun loginUser(context:  Context) =viewModelScope.launch {
         try {
             if (!validateLoginForm()){
                 throw IllegalArgumentException("email and password can not be empty")
             }
-            loginUiState = loginUiState.copy(isloading = true)
+            loginUiState = loginUiState.copy(isLoading = true)
             loginUiState = loginUiState.copy(loginError = null)
             repository.login(
                 loginUiState.userName,
                 loginUiState.password
             ){
-                    isSuccesful ->
-                if (isSuccesful){
-                    Toast.makeText(context, text"success Login", Toast.LENGHT_SHORT).show()
-                    loginUiState = loginUiState.copy(isSucceesLogin = true)
-                }else{
-                    Toast.makeText(context, text"Failed Login", Toast.LENGHT_SHORT).show()
-                    loginUiState = loginUiState.copy(isSucceesLogin = false)
+                    isSuccessful ->
+                if (isSuccessful){
+                    Toast.makeText(context, "Success Login", Toast.LENGTH_SHORT).show()
+                    loginUiState= loginUiState.copy(isSuccessLogin = true)
                 }
+                else {
+                    Toast.makeText(context, "Failed Login", Toast.LENGTH_SHORT).show()
+                    loginUiState= loginUiState.copy(isSuccessLogin = false)
+                }
+            }
 
-            }
-            catch (e:Exception){
-                loginUiState = loginUiState.copy(loginError = e.localizedMessage)
-                e.printStackTrace()
-            }finally {
-                loginUiState =  loginUiState.copy(isLoading = false)
-            }
+        } catch (e: Exception){
+
+            loginUiState = loginUiState.copy(loginError = e.localizedMessage)
+            e.printStackTrace()
+
+        }
+
+        finally {
+            loginUiState = loginUiState.copy(isLoading = false)
         }
     }
+
 }
-}
-data class LoginUiState(
-    val userName:String = "",
+data class  LoginUiState(
+    val userName:String ="",
     val password:String ="",
-    val userNameSignup:String = "",
-    val passwordSignup:String ="",
-    val comfirmpasswordSignup:String ="",
+    val userNameSignUp:String = "",
+    val passwordSignUp:String ="",
+    val confrimPasswordSignUp:String ="",
     val isLoading : Boolean = false,
     val isSuccessLogin:Boolean = false,
     val signUpError:String? = null,
     val loginError:String? = null,
 
-)
+    )
