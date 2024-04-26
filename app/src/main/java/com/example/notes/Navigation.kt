da1798e788b013346898ad8b7d29ca4c0cf0eb1c
Aymiza <sanni.ayomide65@gmailcom>
2024-04-26 21:16:00 +0100

    new commits

--- app/src/main/java/com/example/notes/Navigation.kt
+++ app/src/main/java/com/example/notes/Navigation.kt
@@ -1,10 +1,12 @@
 package com.example.notes
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
 import com.example.notes.home.Home
 import com.example.notes.login.LoginScreen
 import com.example.notes.login.LoginViewModel
@@ -19,6 +21,10 @@ enum class HomeRoutes{
     Home,
     Detail
 }
+enum class  NestedRoutes{
+    Main,
+    Login
+}
 @Composable
 fun Navigation(
 navController: NavHostController = rememberNavController(),
@@ -28,9 +34,16 @@ loginViewModel: LoginViewModel
    NavHost(navController = navController,
        startDestination = LoginRoutes.Signin.name
    ) {
+
+
+
+
+
+
+
         composable(route = LoginRoutes.Signin.name){
             LoginScreen(onNavToHomePage = {
-                navController.navigate(HomeRoutes.Home.name){
+                navController.navigate(NestedRoutes.Main.name){
                     launchSingleTop = true
                     popUpTo(route = LoginRoutes.Signin.name){
 
@@ -51,7 +64,8 @@ loginViewModel: LoginViewModel
         }
 
         composable(route = LoginRoutes.Signup.name){
-            SignUpScreen(onNavToHomePage = {       navController.navigate(HomeRoutes.Home.name){
+            SignUpScreen(onNavToHomePage = {
+                navController.navigate(NestedRoutes.Main.name){
                 popUpTo(LoginRoutes.Signup.name){
                     inclusive = true
                 }
@@ -66,9 +80,26 @@ loginViewModel: LoginViewModel
             }
         }
 
-        composable(route = HomeRoutes.Home.name){
-            Home()
+
+    }
+
+    fun navigation(startDestination: String, route: String) {
+        TODO("Not yet implemented")
+    }
+
+    fun NavGraphBuilder.authGraph
+                (navController: NavHostController,
+                 loginViewModel: LoginViewModel,
+    ) {
+        navigation(
+            startDestination = LoginRoutes.Signin.name,
+            route = NestedRoutes.Login.name
+        ){
+
         }
     }
 
+
 }
+
+
